67327db57802d9db1937469c9f445cbd0f27aa72
Sarthak Aggarwal <84911014+SarthakAggarwal19032001@users.noreply.github.com>
2022-11-09 21:59:20 +0530

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,133 @@
+window.addEventListener("load", initEvents);
+
+let tableHeader = ["Id", "Task Name", "Task Desc", "Task Date", "Select Task"];
+let table;
+let body;
+function initEvents() {
+    table = document.querySelector("#table");
+    document.querySelector("#addTask").addEventListener("click", addTask);
+    document.querySelector("#deleteTask").addEventListener("click", deleteTask);
+    document.querySelector("#sortTask").addEventListener("click", sortTask);
+    document.querySelector("#searchTask").addEventListener("click", searchTask);
+    document.querySelector("#saveTask").addEventListener("click", saveTask);
+    document.querySelector("#loadTask").addEventListener("click", loadTask);
+    document.querySelector("#searchBox").addEventListener("change", loadTask);
+    generateHeader();
+}
+
+function generateHeader() {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for(let key of tableHeader) {
+        let th = document.createElement("th");
+        th.innerHTML = key;
+        row.appendChild(th);
+    }
+    table.appendChild(thead);
+    body = table.createTBody();
+}
+
+function sortTask() {
+    let key = document.querySelector("#sort").value;
+    let order = document.querySelector("#order").value;
+    // console.log(key, order);
+    obj.sortTask(key, order);
+    showTask();
+}
+
+// function addTask() {
+//     let taskName = document.querySelector("#box_1").value;
+//     let taskTitle = document.querySelector("#box_2").value;
+//     let row = table.insertRow();
+//     taskId++;
+//     let column = row.insertCell();
+//     column.innerHTML = taskId;
+
+//     column = row.insertCell();
+//     column.innerHTML = taskName;
+
+//     column = row.insertCell();
+//     column.innerHTML = taskTitle;
+
+//     column = row.insertCell();
+//     let date = new Date();
+//     column.innerHTML = date;
+// }
+
+function addTask() {
+    let taskName = document.querySelector("#box_1");
+    let taskDesc = document.querySelector("#box_2");
+    obj.addTask(taskName.value, taskDesc.value);
+    taskName.value = "";
+    taskDesc.value = "";
+    showTask();
+}
+
+function showTask() {
+    body.innerHTML = "";
+    obj.taskList.forEach(function(task) {
+        let row = body.insertRow();
+        let column = row.insertCell();
+        column.innerHTML = task.id;
+
+        column = row.insertCell();
+        column.innerHTML = task.name;
+
+        column = row.insertCell();
+        column.innerHTML = task.details;
+
+        column = row.insertCell();
+        column.innerHTML = task.date;
+
+        let inputTag = document.createElement("input");
+        inputTag.setAttribute("type","checkbox");
+        inputTag.setAttribute("value", task.id);
+        column = row.insertCell();
+        column.appendChild(inputTag);
+        inputTag.addEventListener("change", selectTask);
+    });
+}
+
+function selectTask() {
+    console.log(this.value);
+    obj.toggleTask(this.value);
+}
+
+function deleteTask() {
+    obj.deleteTask();
+    showTask();
+}
+
+function saveTask() {
+    // check if browser support localstorage
+    if(window.localStorage) {
+        var taskData = JSON.stringify(obj.taskList);
+        localStorage.setItem("taskListData", taskData);
+        alert("Data Saved Successfully");
+    }
+    else {
+        alert("Cannot Save, Browser donot support LocalStorage");
+    }
+}
+
+function loadTask() {
+    if(window.localStorage) {
+        if(localStorage.taskListData) {
+            var data = localStorage.getItem("taskListData");
+            obj.taskList = JSON.parse(data);
+            for(let i in obj.taskList) {
+                obj.id++;
+            }
+            showTask();
+        }
+        else {
+            alert("No Task Available...");
+        }
+    }
+}
+
+function searchTask() {
+    let searchKey = document.querySelector("#searchBox").value;
+    obj.searchTask(searchKey);
+    showTask();
+}
---
+++ modal.js
@@ -0,0 +1,71 @@
+// functions are first class citizen
+// function as a constructor
+// function Task(id, name, details) {
+//     this.id = id;
+//     this.name = name;
+//     this.details = details;
+//     this.date = new Date();
+// }
+class Task {
+    constructor(id, name, details) {
+        this.id = id;
+        this.name = name;
+        this.details = details;
+        this.selected = false;
+        this.date = new Date();
+    }
+}
+
+var obj = {
+    id : 0,
+    taskList : [],
+    addTask : function(name, desc) {
+        this.id++;
+        let taskObj = new Task(this.id, name, desc);
+        this.taskList.push(taskObj);
+        console.log(this.taskList);
+    },
+
+    toggleTask : function(id) {
+        var toggleTask = this.taskList.filter(function(taskObj) {
+            return taskObj.id == id;
+        })
+        // console.log(toggleTask);
+        toggleTask[0].selected = !toggleTask[0].selected;
+        // console.log(toggleTask);
+    },
+
+    deleteTask : function() {
+        this.taskList = this.taskList.filter(function(taskObj) {
+            return taskObj.selected == false;
+        })
+    },
+
+    sortTask : function(key, order) {
+        return this.taskList.sort(function(a,b) {
+            //similar to java comparator
+            let x = a[key];
+            let y = b[key];
+
+            // if(x > y) {
+            //     return -1;
+            // }
+            // if(x < y) {
+            //     return 1;
+            // }
+            // return 0;
+            if(order == "asc") {
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            }
+            else {
+                return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+            }
+        })
+    },
+
+    searchTask : function(searchKey) {
+        this.taskList = this.taskList.filter(function(x) {
+            return x.name.includes(searchKey);
+        });
+    }
+}
